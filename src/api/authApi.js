ebfa4086ffcbb78bd52de24b202d0eab822aa787
Sandesh Kumar Mali <95668744+Sandesh-MCC@users.noreply.github.com>
2026-06-26 21:03:09 +0530

    Update API endpoints for authentication

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,20 +3,20 @@ import axiosInstance from "./axiosInstance";
 
 // Login user
 export const loginUser = async (credentials) => {
-    const response = await axiosInstance.post("https://movie-webapp-production.up.railway.app/api/auth/login", credentials);
+    const response = await axiosInstance.post("https://movie-webapp-backend-ties.onrender.com/api/auth/login", credentials);
     return response.data;
 };
 
 
 // Register user
 export const registerUser = async (userData) => {
-    const response = await axiosInstance.post("https://movie-webapp-production.up.railway.app/api/auth/register", userData);
+    const response = await axiosInstance.post("https://movie-webapp-backend-ties.onrender.com/api/auth/register", userData);
     return response.data;
 };
 
 
 // Get logged-in user profile
 export const getProfile = async () => {
-    const response = await axiosInstance.get("/auth/profile");
+    const response = await axiosInstance.get("https://movie-webapp-backend-ties.onrender.com/auth/profile");
     return response.data;
 };
